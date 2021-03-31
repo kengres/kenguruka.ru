b998d54460adaec5e22a6bfd1ea6719d611dc091
kenguruka grevisse <kenguruka24@gmail.com>
2021-03-31 15:00:06 +0300

    some stuff

--- app.kenguruka/src/main.js
+++ app.kenguruka/src/main.js
@@ -4,6 +4,8 @@ import "./registerServiceWorker";
 import router from "./router";
 import "./assets/styles/index.scss";
 
+console.log(`[MAIN]..`)
+
 createApp(App)
   .use(router)
   .mount("#app");
