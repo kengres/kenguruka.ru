6af4659b5ae9d17859db3db5927065d06b886e40
Grevisse Kenguruka <kengres@yandex.ru>
2022-01-19 08:29:24 +0200

    updated manifest and deploy scripts

--- app/vue.config.js
+++ app/vue.config.js
@@ -4,22 +4,13 @@ module.exports = {
     themeColor: "#08843A",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "#FAE7C6",
-    assetsVersion: "1.0.2",
+    assetsVersion: "1.0.3",
     iconPaths: {
       favicon32: "img/icons/favicon-32x32.png",
       favicon16: "img/icons/favicon-16x16.png",
       appleTouchIcon: "img/icons/apple-touch-icon.png",
       maskIcon: "img/icons/safari-pinned-tab.svg",
       msTileImage: "img/icons/mstile-150x150.png",
-      msTileColor: "#d7f9de",
-    },
-    manifestOptions: {
-      name: "Kando Finances",
-      short_name: "Kando-Fi",
-      start_url: "/",
-      id: "ru.kenguruka.finances",
-      background_color: "#FAE7C6",
-      msTileColor: "#d7f9de",
     },
   },
 };
--- scripts/deploy.client.sh
+++ scripts/deploy.client.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 cd app
 yarn build
-tar czf app.kenguruka.tar.gz dist
-scp -r app.kenguruka.tar.gz kengres@217.25.93.162:~
-rm app.kenguruka.tar.gz
+tar czf finances.kenguruka.tar.gz dist
+scp -r finances.kenguruka.tar.gz kengres@217.25.93.162:~
+rm finances.kenguruka.tar.gz
 
 ssh kengres@217.25.93.162 << 'ENDSSH'
-cd sites/kenguruka/app.kenguruka.ru
+cd sites/kenguruka/finances.kenguruka.ru
 rm -rf dist
 cd ~/
-tar xf app.kenguruka.tar.gz -C sites/kenguruka/app.kenguruka.ru
-rm app.kenguruka.tar.gz
-# cd sites/kenguruka/app.kenguruka.ru
-# yarn install --production
+tar xf finances.kenguruka.tar.gz -C sites/kenguruka/finances.kenguruka.ru
+rm finances.kenguruka.tar.gz
 ENDSSH
